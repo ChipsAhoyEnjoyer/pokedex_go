abb7ca972f603d5d88731ad5ea945a8e85a3df7a
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-03-06 18:25:08 -0500

    main repl logic added into repl.startRepl

--- repl.go
+++ repl.go
@@ -1,6 +1,24 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	user := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		user.Scan()
+		userInput := cleanInput(user.Text())
+		if len(userInput) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %v\n", userInput[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	t := strings.ToLower(text)
