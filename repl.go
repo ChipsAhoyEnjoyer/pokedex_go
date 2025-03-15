943f2cae597f60db592ef08cff2cf02a883de594
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-03-15 16:07:49 -0400

    Added a key event listener for the main repl, changing over to this reader in order to be able to take in special events

--- repl.go
+++ repl.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
 
 func startRepl() {
+	err := keyEventListener()
+	if err != nil {
+		panic(err)
+	}
 	commandRegistry := generateCommandRegistry()
 
 	response := bufio.NewScanner(os.Stdin)
@@ -42,4 +48,46 @@ func cleanInput(text string) []string {
 		s = append(s, "")
 	}
 	return s
+}
+
+func keyEventListener() error {
+	if err := keyboard.Open(); err != nil {
+		return err
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	typing := true
+	input := ""
+	prefix := "Pokedex > "
+
+	for typing {
+		fmt.Print(prefix, input)
+		currLineCharacterCount := len(prefix) + len(input)
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			return err
+		}
+		switch key {
+		case keyboard.KeyEsc:
+			return nil
+		case keyboard.KeyBackspace2:
+			if len(input) > 0 {
+				input = input[:len(input)-1]
+			}
+		default:
+			if key == 0 { // check if key pressed is a special character i.e. ENTER, BACKSPACE, CAPS LOCK, etc.
+				input += string(char)
+			}
+		}
+		if !typing {
+			break
+		}
+		// clear the current line
+		fmt.Print("\r")
+		fmt.Print(strings.Repeat(" ", currLineCharacterCount))
+		fmt.Print("\r")
+	}
+	return nil
 }
