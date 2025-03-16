ebfd529dc747326e06f5f6f2cc64197a118c48cb
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-03-16 13:28:05 -0400

    User history now has more functionality

--- userHistory.go
+++ userHistory.go
@@ -10,15 +10,22 @@ func newUserHistory() *userHistory {
 	return &userHistory{currentIndex: -1, history: []string{}, historyLen: 0}
 }
 
-func (uH *userHistory) get() (element string, success bool) {
-	if uH.historyLen > 0 {
+func (uH *userHistory) add(element string) {
+	uH.history = append(uH.history, element)
+	uH.currentIndex++
+	uH.historyLen++
+}
+
+func (uH *userHistory) decrementIndex() (element string, success bool) {
+	if uH.currentIndex > 0 && uH.historyLen > 0 {
 		return uH.history[uH.currentIndex], true
 	}
 	return "", false
 }
 
-func (uH *userHistory) add(element string) {
-	uH.history = append(uH.history, element)
-	uH.currentIndex++
-	uH.historyLen++
+func (uH *userHistory) incrementIndex() (element string, success bool) {
+	if uH.currentIndex+1 < uH.historyLen {
+		return uH.history[uH.currentIndex], true
+	}
+	return "", false
 }
